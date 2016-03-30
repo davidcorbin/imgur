467ae229106ef389d0bbb22507ff89dc3a24a7c7
David Corbin <daconex@gmail.com>
2016-03-29 23:42:16 -0400

    Base commit. Working single image upload.

---
+++ js/script.js
@@ -0,0 +1,30 @@
+function upload(file) {
+	if (!file || !file.type.match(/image.*/)) return;
+	uploading();
+	var fd = new FormData();
+	fd.append("image", file);
+	fd.append("key", "6528448c258cff474ca9701c5bab6927");
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", "http://api.imgur.com/2/upload.json");
+	xhr.onload = function(){uploaded(JSON.parse(xhr.response))};
+	xhr.send(fd);
+}
+
+/*
+ * Function called when image starts uploading
+ */
+function uploading() {
+    document.querySelector("#link").textContent = "Uploading...";
+}
+
+function uploaded(response) {
+    var original = response.upload.links.original;
+    document.querySelector("#link").innerHTML = "<a href='"+original+"'>"+original+"</a>";
+    document.querySelector("#link a").addEventListener("click", function (e){e.stopPropagation();}, false);
+}
+
+window.onload = function () {
+    // Stop pseudo clicking upload layer when github-corner is clicked
+    document.getElementsByTagName("a")[0].addEventListener("click", function (e){e.stopPropagation();}, false);
+
+};
